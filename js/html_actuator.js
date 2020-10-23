eb53a271587bd8dd9d79c6da02679605ccace6ad
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-23 08:27:53 -0500

    Fix tile-super

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -57,7 +57,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
-  if (tile.value > 32768) classes.push("tile-super");
+  if (tile.value > 67108864) classes.push("tile-super");
 
   this.applyClasses(wrapper, classes);
 
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "니가 이겼다" : "게임 끝";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
